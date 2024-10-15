e2db96fdee0d6852456f526cde07956549ff3807
Silas Bue Handgaard <silasbueh@hotmail.com>
2024-10-15 19:43:35 +0200

    refactor lsp

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -6,17 +6,13 @@ return {
     "neovim/nvim-lspconfig",
     'hrsh7th/nvim-cmp',
     'hrsh7th/cmp-nvim-lsp',
-    "folke/neodev.nvim",
   },
   config = function()
     require("mason").setup()
     require("mason-lspconfig").setup()
 
-    -- Add additional capabilities supported by nvim-cmp
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
-    local wk = require("which-key")
-
     -- add default cmp-nvim-lsp capabilities to each lsp
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
     require("mason-lspconfig").setup_handlers {
       function(server_name)
         require("lspconfig")[server_name].setup {
@@ -26,28 +22,23 @@ return {
     }
 
     vim.api.nvim_create_autocmd('LspAttach', {
-      callback = function(ev)
-        wk.add({
-          { "gD",         vim.lsp.buf.code_action,             mode = { "n" },      desc = "lsp declaration" },
-          { "gd",         vim.lsp.buf.definition,              mode = { "n" },      desc = "lsp definition" },
-          { "K",          vim.lsp.buf.hover,                   mode = { "n" },      desc = "lsp hover" },
-          { "gi",         vim.lsp.buf.implementation,          mode = { "n" },      desc = "lsp implementation" },
-          { "<c-k>",      vim.lsp.buf.signature_help,          mode = { "n" },      desc = "lsp signature_help" },
-          { "<leader>wa", vim.lsp.buf.add_workspace_folder,    mode = { "n" },      desc = "lsp add workspace folder" },
-          { "<leader>wr", vim.lsp.buf.remove_workspace_folder, mode = { "n" },      desc = "lsp remove workspace folder" },
-          { "<leader>D",  vim.lsp.buf.type_definition,         mode = { "n" },      desc = "lsp type definition" },
-          { "<leader>rn", vim.lsp.buf.rename,                  mode = { "n" },      desc = "lsp rename" },
-          { "<leader>.",  vim.lsp.buf.code_action,             mode = { "n", "v" }, desc = "lsp code actions" },
-          { "gr",         vim.lsp.buf.references,              mode = { "n" },      desc = "lsp references" },
-          {
-            "<leader>wl",
-            function()
-              print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-            end,
-            mode = { "n" },
-            desc = "lsp remove workspace folder"
-          },
-        }, { buffer = ev.buf })
+      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+      callback = function(_)
+        local cur_buf = vim.api.nvim_get_current_buf()
+        require("which-key").add({
+          { "gD",         vim.lsp.buf.declaration,             mode = { "n" },      desc = "lsp declaration",             buffer = cur_buf },
+          { "gd",         vim.lsp.buf.definition,              mode = { "n" },      desc = "lsp definition",              buffer = cur_buf },
+          { "K",          vim.lsp.buf.hover,                   mode = { "n" },      desc = "lsp hover",                   buffer = cur_buf },
+          { "gi",         vim.lsp.buf.implementation,          mode = { "n" },      desc = "lsp implementation",          buffer = cur_buf },
+          { "<c-k>",      vim.lsp.buf.signature_help,          mode = { "n" },      desc = "lsp signature_help",          buffer = cur_buf },
+          { "<leader>wa", vim.lsp.buf.add_workspace_folder,    mode = { "n" },      desc = "lsp add workspace folder",    buffer = cur_buf },
+          { "<leader>wr", vim.lsp.buf.remove_workspace_folder, mode = { "n" },      desc = "lsp remove workspace folder", buffer = cur_buf },
+          { "<leader>D",  vim.lsp.buf.type_definition,         mode = { "n" },      desc = "lsp type definition",         buffer = cur_buf },
+          { "<leader>rn", vim.lsp.buf.rename,                  mode = { "n" },      desc = "lsp rename",                  buffer = cur_buf },
+          { "<leader>.",  vim.lsp.buf.code_action,             mode = { "n", "v" }, desc = "lsp code actions",            buffer = cur_buf },
+          { "gr",         vim.lsp.buf.references,              mode = { "n" },      desc = "lsp references",              buffer = cur_buf },
+          -- { "<leader>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, mode = { "n" },      desc = "lsp remove workspace folder", buffer = cur_buf },
+        })
       end,
     })
   end
