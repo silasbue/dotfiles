064317a422475e8a51ebf8dbb6527d70321cdcc4
Silas Bue Handgaard <silasbueh@hotmail.com>
2024-10-21 12:05:48 +0200

    update wezterm config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,44 +1,30 @@
-local wezterm = require("wezterm") --[[@as Wezterm]]
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
--- FONT
+-- allows this config to be changed directly from neovim
+local wezterm_config_nvim = wezterm.plugin.require('https://github.com/winter-again/wezterm-config.nvim')
+wezterm.on('user-var-changed', function(window, pane, name, value)
+  local overrides = window:get_config_overrides() or {}
+  overrides = wezterm_config_nvim.override_user_var(overrides, name, value)
+  window:set_config_overrides(overrides)
+end)
+
+config.color_scheme = 'Gruvbox Dark (Gogh)'
 config.font = wezterm.font('FiraCode Nerd Font')
 config.font_size = 13
-
--- UI
--- config.color_scheme = 'tokyonight_moon'
-config.color_scheme = 'Gruvbox Dark (Gogh)'
--- config.color_scheme = 'Catppuccin Mocha'
-
 config.adjust_window_size_when_changing_font_size = false
-
--- remove title bar
-config.window_decorations = 'RESIZE'
+config.window_decorations = 'RESIZE' -- remove title bar
 config.hide_tab_bar_if_only_one_tab = true
--- config.enable_tab_bar = false
--- set default window size
--- config.use_fancy_tab_bar = false
--- config.tab_bar_at_bottom = true
-config.initial_cols = 142
-config.initial_rows = 44
--- change background opacity
+config.initial_cols = 142            -- startup window sizing
+config.initial_rows = 44             -- -||-
 config.window_background_opacity = 0.95
 config.macos_window_background_blur = 10
--- config.window_padding = {
---   left = 0,
---   right = 0,
---   top = 0,
---   bottom = 0,
--- }
--- BEHAVIOUR
 -- by default (on macos) left option key is treated as the ALT key
 -- this disables that behaviour and treats it as option instead
 config.send_composed_key_when_left_alt_is_pressed = true
 
--- KEY BINDINGS
 config.keys = {
-  -- Prompt for a name to use for a new workspace and switch to it.
-  {
+  { -- Prompt for a name to use for a new workspace and switch to it.
     key = 'w',
     mods = 'CTRL|OPT',
     action = wezterm.action.PromptInputLine {
@@ -48,9 +34,6 @@ config.keys = {
         { Text = 'Enter name for new workspace' },
       },
       action = wezterm.action_callback(function(window, pane, line)
-        -- line will be `nil` if they hit escape without entering anything
-        -- An empty string if they just hit enter
-        -- Or the actual line of text they wrote
         if line then
           window:perform_action(
             wezterm.action.SwitchToWorkspace {
@@ -63,8 +46,7 @@ config.keys = {
     },
   },
   { key = 't', mods = 'OPT', action = wezterm.action.ShowTabNavigator },
-  {
-    -- open interactive list of workspaces
+  { -- open interactive list of workspaces
     key = 'w',
     mods = 'OPT',
     action = wezterm.action.ShowLauncherArgs { flags = 'WORKSPACES|LAUNCH_MENU_ITEMS' },
